7c4bd896b1419bda38aff3127fd787a469dd30ee
MadsPreston <maddyprestn@gmail.com>
2024-08-22 23:07:30 -0400

    Fixed Comma Issue

--- MTG.py
+++ MTG.py
@@ -2,24 +2,21 @@ import tkinter.filedialog
 import csv
 
 def openfile():
-   filename = tkinter.filedialog.askopenfile(mode='r')
-   return filename.name
+    filename = tkinter.filedialog.askopenfile(mode='r')
+    return filename.name
 
-def readCSV(fileName):
-    listName = []
-    with open(fileName, 'r') as csvfile:
-        # creating a csv reader object
-        csvreader = csv.reader(csvfile)
-
-        # extracting each data row one by one
-        for row in csvreader:
-            listName.append(row)
-    return listName
+def readDeck(fileName):
+    f = open(fileName, "r")
+    deck = f.read()
+    f.close()
+    return deck
 
 def importDeck():
-    return readCSV(openfile())
+    return readDeck(openfile())
 
 def main():
-    print(importDeck())
+    deck = importDeck()
+    print(deck.split('\n'))
+
 
 main()
